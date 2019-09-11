6a6f97776f3dbb684ed69418df4347cebbecd8e7
Shawn Adams <shawn@clarq.dev>
2019-09-10 21:59:24 -0400

    Initial pass at skateboard model. Generated associated migration file.

--- src/skateboard_rest_api/boards/models.py
+++ src/skateboard_rest_api/boards/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class SkateBoard(models.Model):
+    owner = models.CharField(max_length=70)
+    brand = models.CharField(max_length=70)
+    weight = models.FloatField()
+    location = models.CharField(max_length=70)
--- src/skateboard_rest_api/settings/base.py
+++ src/skateboard_rest_api/settings/base.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'skateboard_rest_api.boards',
 ]
 
 MIDDLEWARE = [
