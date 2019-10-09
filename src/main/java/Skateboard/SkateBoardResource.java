8f352243cb79a969ff5f91f9ff0d1b801d7f7089
Nandhinee Kandasamy <knandhinee@gmail.com>
2019-10-08 21:22:57 -0400

    Skateboard Project Setup

---
+++ src/main/java/Skateboard/SkateBoardResource.java
@@ -0,0 +1,21 @@
+package Skateboard;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+
+@Path("hello")
+public class SkateBoardResource {
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayhello() {
+		return "hello";
+	}
+
+
+
+}
