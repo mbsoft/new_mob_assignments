c82e75c99bc15c9d31db34eeb1b41714cbfb1f51
Venkat Velicheti <kiranvelicheti@gmail.com>
2020-01-16 18:06:13 -0500

    Added test and implementation for verifying repository call for fetching all skateboards

--- src/main/java/com/denso/demo/service/SkateBoardServiceImpl.java
+++ src/main/java/com/denso/demo/service/SkateBoardServiceImpl.java
@@ -1,6 +1,7 @@
 package com.denso.demo.service;
 
 import com.denso.demo.domain.SkateBoard;
+import com.denso.demo.repository.SkateBoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +11,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SkateBoardServiceImpl implements SkateBoardService {
 
+    private final SkateBoardRepository skateBoardRepository;
+
     @Override
     public List<SkateBoard> getAllAvailableSkateBoards() {
+        skateBoardRepository.findAll();
         return null;
     }
 }
--- src/test/java/com/denso/demo/service/SkateBoardServiceImplTest.java
+++ src/test/java/com/denso/demo/service/SkateBoardServiceImplTest.java
@@ -1,20 +1,25 @@
 package com.denso.demo.service;
 
 import com.denso.demo.domain.SkateBoard;
+import com.denso.demo.repository.SkateBoardRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SkateBoardServiceImplTest {
 
     @InjectMocks
     private SkateBoardServiceImpl classUnderTest;
+    @Mock
+    private SkateBoardRepository skateBoardRepository;
 
     @Test
     public void getAllAvailableSkateBoards() {
@@ -22,5 +27,6 @@ public class SkateBoardServiceImplTest {
         List<SkateBoard> actual = classUnderTest.getAllAvailableSkateBoards();
 
         assertThat(actual).isNull();
+        verify(skateBoardRepository).findAll();
     }
 }
