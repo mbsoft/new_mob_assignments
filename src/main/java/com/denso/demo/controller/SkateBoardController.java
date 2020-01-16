7df3740f9593ff2ef9e039562c6f6d06da5bb0fb
Venkat Velicheti <kiranvelicheti@gmail.com>
2020-01-16 18:43:39 -0500

    Added test to verify service call

--- src/main/java/com/denso/demo/controller/SkateBoardController.java
+++ src/main/java/com/denso/demo/controller/SkateBoardController.java
@@ -1,6 +1,8 @@
 package com.denso.demo.controller;
 
 import com.denso.demo.domain.SkateBoard;
+import com.denso.demo.service.SkateBoardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +11,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/skate-board")
+@RequiredArgsConstructor
 public class SkateBoardController {
+
+    private final SkateBoardService skateBoardService;
 
     @GetMapping
     public List<SkateBoard> getSkateBoards() {
-        return null;
+        return skateBoardService.getAllAvailableSkateBoards();
     }
 }
--- src/test/java/com/denso/demo/controller/SkateBoardControllerTest.java
+++ src/test/java/com/denso/demo/controller/SkateBoardControllerTest.java
@@ -1,21 +1,30 @@
 package com.denso.demo.controller;
 
+import com.denso.demo.service.SkateBoardService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SkateBoardControllerTest {
 
+    @Mock
+    private SkateBoardService skateBoardService;
+
     @Test
     public void getSkateBoards_givenAPIPath_return200AsStatusCode() {
+
         given()
-                .standaloneSetup(SkateBoardController.class)
+                .standaloneSetup(new SkateBoardController(skateBoardService))
                 .when()
                 .get("/skate-board")
                 .then()
                 .statusCode(200);
+
+        verify(skateBoardService).getAllAvailableSkateBoards();
     }
 }
