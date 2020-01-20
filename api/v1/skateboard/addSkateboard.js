a3e8b73ad3dff76c6a2779225a02559c4b3fb2a8
Jim Welch <j.gauntlett.welch@gmail.com>
2020-01-20 11:53:54 -0500

    Logging
    Added more rudimentary logging

--- api/v1/skateboard/addSkateboard.js
+++ api/v1/skateboard/addSkateboard.js
@@ -1,9 +1,11 @@
 const mongo = require('../../../data/mongo_mock_db');
+const logger = require('../../../logger');
 
 const addSkateboard = (req, res) => {
 
+    logger.info('POST addSkateboard called');
     mongo.collection('skateboards').insert(req.body,function(err){
-        console.log(err);
+        logger.info(err);
     });
 
     res.json(req.body)
