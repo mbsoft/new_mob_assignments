eebd2d25d86c5d12597c27c6afdf2b4ac3746022
dongrerohan421 <dongrerohan421@gmail.com>
2020-02-08 09:26:20 -0500

    create spring boot maven project skeleton

---
+++ src/main/java/com.detroitlabs.skateboard/NewMobAssignmentsApplication.java
@@ -0,0 +1,13 @@
+package com.detroitlabs.skateboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewMobAssignmentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewMobAssignmentsApplication.class, args);
+    }
+
+}
