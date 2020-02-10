782df175d16ad356dd3007348a5c556031a80b52
dongrerohan421 <dongrerohan421@gmail.com>
2020-02-09 22:27:02 -0500

    add logger

--- src/main/java/com/detroitlabs/skateboard/controller/SkateBoardResource.java
+++ src/main/java/com/detroitlabs/skateboard/controller/SkateBoardResource.java
@@ -3,6 +3,7 @@ package com.detroitlabs.skateboard.controller;
 import com.detroitlabs.skateboard.exception.SkateBoardNotFoundException;
 import com.detroitlabs.skateboard.model.SkateBoard;
 import com.detroitlabs.skateboard.service.SkateBoardServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/")
+@Slf4j
 public class SkateBoardResource {
 
     @Autowired
@@ -22,6 +24,7 @@ public class SkateBoardResource {
     @PostMapping(path = "createSkateboard")
     public ResponseEntity<Object> createSkateBoard(@Valid @RequestBody SkateBoard skateBoard) {
         SkateBoard createSkateBoard = skateBoardServiceImpl.save(skateBoard);
+        log.info("SkateBoard created with id: " + createSkateBoard.getId());
 
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequestUri()
@@ -29,6 +32,7 @@ public class SkateBoardResource {
                 .buildAndExpand(createSkateBoard.getId())
                 .toUri();
 
+        log.info("location to fetch SkateBoard with id " + createSkateBoard.getId() + " is: " + location);
         return ResponseEntity.created(location).build();
     }
 
@@ -36,9 +40,12 @@ public class SkateBoardResource {
     public SkateBoard retrieveSkateBoard(@PathVariable Integer id) {
         SkateBoard skateBoard = skateBoardServiceImpl.findOne(id);
 
-        if (skateBoard == null)
+        if (skateBoard == null) {
+            log.info("SkateBoard not found with id: " + id);
             throw new SkateBoardNotFoundException("SkateBoard not found with id " + id);
+        }
 
+        log.info("SkateBoard is found with id: " + id);
         return skateBoard;
     }
 
@@ -51,9 +58,12 @@ public class SkateBoardResource {
     public SkateBoard retrieveSkateBoardByLength(@PathVariable Integer length) {
         SkateBoard skateBoard = skateBoardServiceImpl.findByLength(length);
 
-        if (skateBoard == null)
+        if (skateBoard == null) {
+            log.info("SkateBoard not found with length: " + length);
             throw new SkateBoardNotFoundException("SkateBoard not found with length " + length);
+        }
 
+        log.info("SkateBoard is found with length: " + length);
         return skateBoard;
     }
 
@@ -61,9 +71,12 @@ public class SkateBoardResource {
     public SkateBoard retrieveSkateBoardByWeight(@PathVariable Integer weight) {
         SkateBoard skateBoard = skateBoardServiceImpl.findByWeight(weight);
 
-        if (skateBoard == null)
+        if (skateBoard == null) {
+            log.info("SkateBoard not found with weight: " + weight);
             throw new SkateBoardNotFoundException("SkateBoard not found with weight " + weight);
+        }
 
+        log.info("SkateBoard is found with weight: " + weight);
         return skateBoard;
     }
 
@@ -71,9 +84,12 @@ public class SkateBoardResource {
     public SkateBoard retrieveSkateBoardByBrand(@PathVariable String brand) {
         SkateBoard skateBoard = skateBoardServiceImpl.findByBrand(brand);
 
-        if (skateBoard == null)
+        if (skateBoard == null) {
+            log.info("SkateBoard not found with brand: " + brand);
             throw new SkateBoardNotFoundException("SkateBoard not found with brand " + brand);
+        }
 
+        log.info("SkateBoard is found with brand: " + brand);
         return skateBoard;
     }
 
@@ -81,9 +97,12 @@ public class SkateBoardResource {
     public ResponseEntity<Object> deleteSkateBoard(@PathVariable Integer id) {
         SkateBoard skateBoard = skateBoardServiceImpl.deleteSkateBoardById(id);
 
-        if (skateBoard == null)
+        if (skateBoard == null) {
+            log.info("SkateBoard not found with id: " + id);
             throw new SkateBoardNotFoundException("SkateBoard not found with id " + id);
+        }
 
+        log.info("SkateBoard is deleted with id: " + id);
         return ResponseEntity.status(200).build();
     }
 
@@ -93,15 +112,17 @@ public class SkateBoardResource {
                                                        @PathVariable(value = "brand") String brand,
                                                        @PathVariable(value = "length") Integer length,
                                                        @PathVariable(value = "weight") Integer weight,
-                                                       @PathVariable(value = "isAvailable") Boolean isAvailable
-    ) {
+                                                       @PathVariable(value = "isAvailable") Boolean isAvailable) {
         SkateBoard skateBoard = skateBoardServiceImpl.findOne(id);
 
-        if (skateBoard == null)
+        if (skateBoard == null) {
+            log.info("SkateBoard not found with id: " + id);
             throw new SkateBoardNotFoundException("SkateBoard not found with id " + id);
+        }
 
-        SkateBoard updateSkateBoard = skateBoardServiceImpl.updateSkateBoardDetails(id, ownerName, brand,  length, weight, isAvailable);
+        SkateBoard updateSkateBoard = skateBoardServiceImpl.updateSkateBoardDetails(id, ownerName, brand, length, weight, isAvailable);
 
+        log.info("SkateBoard details updated for id: " + skateBoard.getId());
         return ResponseEntity.ok(updateSkateBoard);
     }
 }
